32a1a8c6b9124eab1091e3db1f0413a7393e621a
nsneha16 <snehanamdeo161705@gmail.com>
2026-07-12 03:06:54 +0530

    fix: add SSL and port for Aiven DB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,8 +6,12 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,   // connection pool, not single connection
+  ssl: {                             // 👈 add karo
+    rejectUnauthorized: true
+  }
 });
   
 
